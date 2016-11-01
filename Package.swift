28f3edaa1c7faa55b987eefc27bbc84cab67173d
Helge Heß <me@helgehess.eu>
2016-11-01 23:07:10 +0100

    Hm, pkgconfig?

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
 import PackageDescription
 
+// https://github.com/apple/swift-evolution/blob/master/proposals/0063-swiftpm-system-module-search-paths.md
+
+
 let package = Package(
-  name: "COpenSSL11Brew"
+  name: "COpenSSL11Brew",
+  pkgConfigName: "openssl",
+  providers: [
+    .Brew(installName: "openssl@1.1")
+  ]
 )
