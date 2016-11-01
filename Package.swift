1c34a64070e1b8e90b407cb4bf1934c889a054ed
Helge Heß <me@helgehess.eu>
2016-11-01 23:10:41 +0100

    Mo' naming fixes

--- Package.swift
+++ Package.swift
@@ -2,11 +2,14 @@ import PackageDescription
 
 // https://github.com/apple/swift-evolution/blob/master/proposals/0063-swiftpm-system-module-search-paths.md
 
+// https://github.com/apple/swift-package-manager/blob/master/Documentation/Reference.md
+
+// PKG_CONFIG_PATH is used
 
 let package = Package(
   name: "COpenSSL11Brew",
   pkgConfig: "openssl",
   providers: [
-    .Brew(installName: "openssl@1.1")
+    .Brew("openssl@1.1")
   ]
 )
